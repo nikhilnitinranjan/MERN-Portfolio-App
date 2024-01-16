df4d1daa4a954e260feafa4ba4c8c7373533bb93
Nikhil Nitin Ranjan <72209132+nikhilnitinranjan@users.noreply.github.com>
2024-01-16 21:39:24 +0530

    initial commit newone

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require ('express')
 const cors = require ('cors')
 const dotenv = require ('dotenv')
+const path = require ('path')
 
 //dotenv config
 dotenv.config()
@@ -12,13 +13,20 @@ const app = express()
 //middleware
 app.use(cors())
 app.use(express.json())
-
+//static folder
+app.use(express.static(path.join(__dirname, './client/build')))
 
 //routes
 // app.get('/', (req, res) => {
 //     res.send('<h1>hello world</h1>')
 // })
 app.use('/api/v1/portfolios', require('./routes/portfiloRoute'))
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, './client/build/index.html'))
+}
+)
+
 
 //port
 const PORT = process.env.PORT || 8080
